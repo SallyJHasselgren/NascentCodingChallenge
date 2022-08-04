ae383af69e6a50fd02ee4d8612f2932e272f7664
Sally Hasselgren <sallyhasselgren@sallys-air.local>
2022-08-04 09:05:29 -0400

    Endpoint now returning 1000 pokemon

--- src/services/pokemonAPI.js
+++ src/services/pokemonAPI.js
@@ -2,6 +2,6 @@ import customAxios from "../utils/customAxios";
 
 export const getPokemon = async () => {
 
-    return await customAxios.get("/api/v2/stat/2");
+    return await customAxios.get("/api/v2/pokemon/?limit=1000");
 
 }
